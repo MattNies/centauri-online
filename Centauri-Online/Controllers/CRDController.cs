15c1de450b22fe9b0663eefbc291d2ff500102c6
Matt Nies <mattcnies@gmail.com>
2018-06-07 02:41:18 -0400

    beginnig crd

---
+++ Centauri-Online/Controllers/CRDController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Centauri_Online.Controllers
+{
+    public class CRDController : Controller
+    {
+        // GET: CRD
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Weapon
+        public ActionResult Weapon()
+        {
+            return View();
+        }
+    }
+}

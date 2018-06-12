ef8a2bbe7a64d79c0eee141f2d2e1b3bdcea5157
Matt Nies <mattcnies@gmail.com>
2018-06-12 02:13:50 -0400

    basic views and controller to find/add/remove/edit weapons

---
+++ Centauri-Online/Data/ConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centauri_Online.Data
+{
+    public class ConnectionHelper
+    {
+        public static String DBFileName
+        {
+            get
+            {
+                return @"CentauriOnline.db";
+            }
+        }
+    }
+}

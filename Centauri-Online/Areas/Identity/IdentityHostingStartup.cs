f8a02aefe9e9a2f6bc8dbf13fc722a28745968b3
Matt Nies <mattcnies@gmail.com>
2018-09-22 14:11:46 -0400

    added identity scaffolding

---
+++ Centauri-Online/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Centauri_Online.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Centauri_Online.Areas.Identity.IdentityHostingStartup))]
+namespace Centauri_Online.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityDataContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityDataContext>();
+            });
+        }
+    }
+}
